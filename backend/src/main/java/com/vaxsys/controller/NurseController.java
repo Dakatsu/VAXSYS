bf3d9ed287b5808d20ac38c451eeba0e9c410a85
Yongshi Liang <waterleung25@gmail.com>
2022-06-08 15:09:38 -0400

    add findDisease to PatientController and NurseController

--- backend/src/main/java/com/vaxsys/controller/NurseController.java
+++ backend/src/main/java/com/vaxsys/controller/NurseController.java
@@ -1,8 +1,12 @@
 package com.vaxsys.controller;
 
+import com.vaxsys.dto.DiseaseDto;
 import com.vaxsys.dto.VaccineDto;
+import com.vaxsys.entity.Disease;
 import com.vaxsys.entity.Vaccine;
+import com.vaxsys.mapper.DiseaseMapper;
 import com.vaxsys.mapper.VaccineMapper;
+import com.vaxsys.repository.DiseaseRepository;
 import com.vaxsys.repository.VaccineRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -17,9 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class NurseController {
 
     private final VaccineRepository vaccineRepository;
+    private final DiseaseRepository diseaseRepository;
 
-    public NurseController(VaccineRepository vaccineRepository) {
+    public NurseController(VaccineRepository vaccineRepository, DiseaseRepository diseaseRepository) {
         this.vaccineRepository = vaccineRepository;
+        this.diseaseRepository = diseaseRepository;
     }
 
     @GetMapping("/vaccine/{name}")
@@ -31,5 +37,16 @@ public class NurseController {
     public Page<VaccineDto> findAllVaccines(Pageable pageable) {
         Page<Vaccine> vaccinePage = vaccineRepository.findAll(pageable);
         return new PageImpl<>(VaccineMapper.INSTANCE.map(vaccinePage.getContent()), pageable, vaccinePage.getTotalElements());
+    }
+
+    @GetMapping("/disease/{name}")
+    public DiseaseDto findDiseaseByName(@PathVariable String name) {
+        return DiseaseMapper.INSTANCE.map(diseaseRepository.findByName(name));
+    }
+
+    @GetMapping("/disease")
+    public Page<DiseaseDto> findAllDiseases(Pageable pageable) {
+        Page<Disease> diseasePage = diseaseRepository.findAll(pageable);
+        return new PageImpl<>(DiseaseMapper.INSTANCE.map(diseasePage.getContent()), pageable, diseasePage.getTotalElements());
     }
 }
--- backend/src/main/java/com/vaxsys/controller/PatientController.java
+++ backend/src/main/java/com/vaxsys/controller/PatientController.java
@@ -1,8 +1,15 @@
 package com.vaxsys.controller;
 
+import com.vaxsys.dto.DiseaseDto;
 import com.vaxsys.dto.VaccineDto;
+import com.vaxsys.entity.Disease;
+import com.vaxsys.mapper.DiseaseMapper;
 import com.vaxsys.mapper.VaccineMapper;
+import com.vaxsys.repository.DiseaseRepository;
 import com.vaxsys.repository.VaccineRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class PatientController {
 
     private final VaccineRepository vaccineRepository;
+    private final DiseaseRepository diseaseRepository;
 
-    public PatientController(VaccineRepository vaccineRepository) {
+    public PatientController(VaccineRepository vaccineRepository, DiseaseRepository diseaseRepository) {
         this.vaccineRepository = vaccineRepository;
+        this.diseaseRepository = diseaseRepository;
     }
 
     @GetMapping("/vaccine/{name}")
     public VaccineDto findVaccineByName(@PathVariable String name) {
         return VaccineMapper.INSTANCE.map(vaccineRepository.findByName(name));
+    }
+
+    @GetMapping("/disease/{name}")
+    public DiseaseDto findDiseaseByName(@PathVariable String name) {
+        return DiseaseMapper.INSTANCE.map(diseaseRepository.findByName(name));
+    }
+
+    @GetMapping("/disease")
+    public Page<DiseaseDto> findAllDiseases(Pageable pageable) {
+        Page<Disease> diseasePage = diseaseRepository.findAll(pageable);
+        return new PageImpl<>(DiseaseMapper.INSTANCE.map(diseasePage.getContent()), pageable, diseasePage.getTotalElements());
     }
 }
