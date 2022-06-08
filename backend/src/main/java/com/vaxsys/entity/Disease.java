eb60024c4fd4af2dcb919fdeaa65559aa67fc05b
Yongshi Liang <waterleung25@gmail.com>
2022-06-08 14:48:47 -0400

    add Disease entity and table

---
+++ backend/src/main/java/com/vaxsys/entity/Disease.java
@@ -0,0 +1,48 @@
+package com.vaxsys.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "disease")
+public class Disease {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Disease() {
+    }
+
+    public Disease(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
