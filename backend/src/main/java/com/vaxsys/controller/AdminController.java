183dfd65382ee1bd6f184f9e9f0d662224617330
Yongshi Liang <waterleung25@gmail.com>
2022-06-08 15:15:27 -0400

    add findVaccine and findDisease to AdminController

--- backend/src/main/java/com/vaxsys/controller/AdminController.java
+++ backend/src/main/java/com/vaxsys/controller/AdminController.java
@@ -1,15 +1,20 @@
 package com.vaxsys.controller;
 
-import com.vaxsys.dto.AccountDto;
-import com.vaxsys.dto.AccountCreationDto;
+import com.vaxsys.dto.*;
 import com.vaxsys.entity.Account;
+import com.vaxsys.entity.Disease;
+import com.vaxsys.entity.Vaccine;
 import com.vaxsys.mapper.AccountMapper;
+import com.vaxsys.mapper.DiseaseMapper;
+import com.vaxsys.mapper.VaccineMapper;
+import com.vaxsys.repository.DiseaseRepository;
+import com.vaxsys.repository.VaccineRepository;
 import com.vaxsys.service.AccountService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -21,9 +26,13 @@ import static com.vaxsys.constant.Role.ADMIN_CREATION_ROLES;
 public class AdminController {
 
     private final AccountService accountService;
+    private final VaccineRepository vaccineRepository;
+    private final DiseaseRepository diseaseRepository;
 
-    public AdminController(AccountService accountService) {
+    public AdminController(AccountService accountService, VaccineRepository vaccineRepository, DiseaseRepository diseaseRepository) {
         this.accountService = accountService;
+        this.vaccineRepository = vaccineRepository;
+        this.diseaseRepository = diseaseRepository;
     }
 
     @PostMapping("/account")
@@ -41,5 +50,27 @@ public class AdminController {
             return null;
         }
         return AccountMapper.INSTANCE.map(account);
+    }
+
+    @PostMapping("/vaccine")
+    public VaccineDto createVaccine(@RequestBody VaccineCreationDto vaccineCreationDto) {
+        Vaccine vaccine = new Vaccine(vaccineCreationDto.getName(), vaccineCreationDto.getDescription());
+        return VaccineMapper.INSTANCE.map(vaccineRepository.save(vaccine));
+    }
+
+    @GetMapping("/vaccine/{name}")
+    public VaccineDto findVaccineByName(@PathVariable String name) {
+        return VaccineMapper.INSTANCE.map(vaccineRepository.findByName(name));
+    }
+
+    @GetMapping("/disease/{name}")
+    public DiseaseDto findDiseaseByName(@PathVariable String name) {
+        return DiseaseMapper.INSTANCE.map(diseaseRepository.findByName(name));
+    }
+
+    @GetMapping("/disease")
+    public Page<DiseaseDto> findAllDiseases(Pageable pageable) {
+        Page<Disease> diseasePage = diseaseRepository.findAll(pageable);
+        return new PageImpl<>(DiseaseMapper.INSTANCE.map(diseasePage.getContent()), pageable, diseasePage.getTotalElements());
     }
 }
