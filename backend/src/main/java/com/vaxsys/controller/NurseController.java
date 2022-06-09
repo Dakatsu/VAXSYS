6956cc5a20107940b6ce678924ff76f346fe76b4
Yongshi Liang <waterleung25@gmail.com>
2022-06-09 10:15:03 -0400

    update nurse/patient controller for using services

--- backend/src/main/java/com/vaxsys/controller/NurseController.java
+++ backend/src/main/java/com/vaxsys/controller/NurseController.java
@@ -8,6 +8,8 @@ import com.vaxsys.mapper.DiseaseMapper;
 import com.vaxsys.mapper.VaccineMapper;
 import com.vaxsys.repository.DiseaseRepository;
 import com.vaxsys.repository.VaccineRepository;
+import com.vaxsys.service.DiseaseService;
+import com.vaxsys.service.VaccineService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -20,33 +22,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/nurse")
 public class NurseController {
 
-    private final VaccineRepository vaccineRepository;
-    private final DiseaseRepository diseaseRepository;
+    private final VaccineService vaccineService;
+    private final DiseaseService diseaseService;
 
-    public NurseController(VaccineRepository vaccineRepository, DiseaseRepository diseaseRepository) {
-        this.vaccineRepository = vaccineRepository;
-        this.diseaseRepository = diseaseRepository;
+    public NurseController(VaccineService vaccineService, DiseaseService diseaseService) {
+        this.vaccineService = vaccineService;
+        this.diseaseService = diseaseService;
     }
 
     @GetMapping("/vaccine/{name}")
     public VaccineDto findVaccineByName(@PathVariable String name) {
-        return VaccineMapper.INSTANCE.map(vaccineRepository.findByName(name));
+        return VaccineMapper.INSTANCE.map(vaccineService.findByName(name));
     }
 
     @GetMapping("/vaccine")
     public Page<VaccineDto> findAllVaccines(Pageable pageable) {
-        Page<Vaccine> vaccinePage = vaccineRepository.findAll(pageable);
+        Page<Vaccine> vaccinePage = vaccineService.findAll(pageable);
         return new PageImpl<>(VaccineMapper.INSTANCE.map(vaccinePage.getContent()), pageable, vaccinePage.getTotalElements());
     }
 
     @GetMapping("/disease/{name}")
     public DiseaseDto findDiseaseByName(@PathVariable String name) {
-        return DiseaseMapper.INSTANCE.map(diseaseRepository.findByName(name));
+        return DiseaseMapper.INSTANCE.map(diseaseService.findByName(name));
     }
 
     @GetMapping("/disease")
     public Page<DiseaseDto> findAllDiseases(Pageable pageable) {
-        Page<Disease> diseasePage = diseaseRepository.findAll(pageable);
+        Page<Disease> diseasePage = diseaseService.findAll(pageable);
         return new PageImpl<>(DiseaseMapper.INSTANCE.map(diseasePage.getContent()), pageable, diseasePage.getTotalElements());
     }
 }
--- backend/src/main/java/com/vaxsys/controller/PatientController.java
+++ backend/src/main/java/com/vaxsys/controller/PatientController.java
@@ -3,10 +3,13 @@ package com.vaxsys.controller;
 import com.vaxsys.dto.DiseaseDto;
 import com.vaxsys.dto.VaccineDto;
 import com.vaxsys.entity.Disease;
+import com.vaxsys.entity.Vaccine;
 import com.vaxsys.mapper.DiseaseMapper;
 import com.vaxsys.mapper.VaccineMapper;
 import com.vaxsys.repository.DiseaseRepository;
 import com.vaxsys.repository.VaccineRepository;
+import com.vaxsys.service.DiseaseService;
+import com.vaxsys.service.VaccineService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -19,27 +22,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/patient")
 public class PatientController {
 
-    private final VaccineRepository vaccineRepository;
-    private final DiseaseRepository diseaseRepository;
+    private final VaccineService vaccineService;
+    private final DiseaseService diseaseService;
 
-    public PatientController(VaccineRepository vaccineRepository, DiseaseRepository diseaseRepository) {
-        this.vaccineRepository = vaccineRepository;
-        this.diseaseRepository = diseaseRepository;
+    public PatientController(VaccineService vaccineService, DiseaseService diseaseService) {
+        this.vaccineService = vaccineService;
+        this.diseaseService = diseaseService;
     }
 
     @GetMapping("/vaccine/{name}")
     public VaccineDto findVaccineByName(@PathVariable String name) {
-        return VaccineMapper.INSTANCE.map(vaccineRepository.findByName(name));
+        return VaccineMapper.INSTANCE.map(vaccineService.findByName(name));
+    }
+
+    @GetMapping("/vaccine")
+    public Page<VaccineDto> findAllVaccines(Pageable pageable) {
+        Page<Vaccine> vaccinePage = vaccineService.findAll(pageable);
+        return new PageImpl<>(VaccineMapper.INSTANCE.map(vaccinePage.getContent()), pageable, vaccinePage.getTotalElements());
     }
 
     @GetMapping("/disease/{name}")
     public DiseaseDto findDiseaseByName(@PathVariable String name) {
-        return DiseaseMapper.INSTANCE.map(diseaseRepository.findByName(name));
+        return DiseaseMapper.INSTANCE.map(diseaseService.findByName(name));
     }
 
     @GetMapping("/disease")
     public Page<DiseaseDto> findAllDiseases(Pageable pageable) {
-        Page<Disease> diseasePage = diseaseRepository.findAll(pageable);
+        Page<Disease> diseasePage = diseaseService.findAll(pageable);
         return new PageImpl<>(DiseaseMapper.INSTANCE.map(diseasePage.getContent()), pageable, diseasePage.getTotalElements());
     }
 }
