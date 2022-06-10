5e4d6f9bab4b06e37d6ea97e5db5871b8d076a59
Kyle Taylor Lange <dakatsu@gmail.com>
2022-06-09 23:18:45 -0400

    Added mapping to a single slot.

--- backend/src/main/java/com/vaxsys/mapper/SlotMapper.java
+++ backend/src/main/java/com/vaxsys/mapper/SlotMapper.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface SlotMapper {
     SlotMapper INSTANCE = Mappers.getMapper(SlotMapper.class);
 
+    SlotDto map(Slot source);
     List<SlotDto> map(List<Slot> source);
 
 }
