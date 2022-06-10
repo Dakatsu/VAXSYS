4b9aac9256fa905f0d1811bc36a2ae27ec107529
Kyle Taylor Lange <dakatsu@gmail.com>
2022-06-09 23:20:10 -0400

    Nurses can create slots.

--- backend/src/main/java/com/vaxsys/controller/NurseController.java
+++ backend/src/main/java/com/vaxsys/controller/NurseController.java
@@ -1,20 +1,26 @@
 package com.vaxsys.controller;
 
-import com.vaxsys.dto.DiseaseDto;
-import com.vaxsys.dto.VaccineDto;
+import com.vaxsys.dto.*;
 import com.vaxsys.entity.Disease;
 import com.vaxsys.entity.Appointment;
+import com.vaxsys.entity.Slot;
 import com.vaxsys.entity.Vaccine;
 import com.vaxsys.mapper.DiseaseMapper;
+import com.vaxsys.mapper.SlotMapper;
 import com.vaxsys.mapper.VaccineMapper;
 import com.vaxsys.service.AppointmentService;
+import com.vaxsys.service.SlotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.vaxsys.service.DiseaseService;
 import com.vaxsys.service.VaccineService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/nurse")
@@ -22,13 +28,15 @@ public class NurseController {
 
     private final VaccineService vaccineService;
     private final DiseaseService diseaseService;
+    private final SlotService slotService;
 
     @Autowired
     private AppointmentService appointmentService;
 
-    public NurseController(VaccineService vaccineService, DiseaseService diseaseService) {
+    public NurseController(VaccineService vaccineService, DiseaseService diseaseService, SlotService slotService) {
         this.vaccineService = vaccineService;
         this.diseaseService = diseaseService;
+        this.slotService = slotService;
     }
 
     @GetMapping("/vaccine/{name}")
@@ -57,5 +65,17 @@ public class NurseController {
     public Page<DiseaseDto> findAllDiseases(Pageable pageable) {
         Page<Disease> diseasePage = diseaseService.findAll(pageable);
         return new PageImpl<>(DiseaseMapper.INSTANCE.map(diseasePage.getContent()), pageable, diseasePage.getTotalElements());
+    }
+
+    @PostMapping("/slot")
+    public SlotDto createSlot(@RequestBody SlotCreationDto slotCreationDto, HttpServletResponse response) throws IOException {
+        Slot slot;
+        try {
+            slot = slotService.createSlot(slotCreationDto);
+        } catch(Exception e) {
+            response.sendError(HttpStatus.FORBIDDEN.value(), "Unable to create slot: " + e.getMessage());
+            return null;
+        }
+        return SlotMapper.INSTANCE.map(slot);
     }
 }
