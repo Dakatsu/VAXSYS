6fa7ea853bd05dbdd1f96662f1cc6984ff41ead0
Kyle Taylor Lange <dakatsu@gmail.com>
2022-06-09 23:19:35 -0400

    Added VaccineCenter to DTO.

--- backend/src/main/java/com/vaxsys/dto/SlotDto.java
+++ backend/src/main/java/com/vaxsys/dto/SlotDto.java
@@ -6,11 +6,21 @@ import java.util.Objects;
 public class SlotDto implements Serializable {
     private String time;
 
+    private VaccineCenterDto vaccineCenter;
+
     public String getTime() {
         return time;
     }
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    public VaccineCenterDto getVaccineCenter() {
+        return vaccineCenter;
+    }
+
+    public void setVaccineCenter(VaccineCenterDto vaccineCenter) {
+        this.vaccineCenter = vaccineCenter;
     }
 }
